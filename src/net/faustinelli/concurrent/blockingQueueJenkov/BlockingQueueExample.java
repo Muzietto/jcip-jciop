279db2f0674c97cd825c6d76aee8f57a86ee7aaf
Muzietto <marco_faustinelli@yahoo.com>
2016-03-28 14:54:38 +0200

    jenkov running

--- src/net/faustinelli/concurrent/blockingQueueJenkov/BlockingQueueExample.java
+++ src/net/faustinelli/concurrent/blockingQueueJenkov/BlockingQueueExample.java
@@ -16,9 +16,15 @@ import java.util.concurrent.BlockingQueue;
  * Created by Marco Faustinelli (Muzietto) on 28/03/2016.
  */
 public class BlockingQueueExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         BlockingQueue queue = new ArrayBlockingQueue(1024);
 
-        Producer produces = new Producer(queue);
+        Producer producer = new Producer(queue);
+        Consumer consumer = new Consumer(queue);
+
+        new Thread(producer).start();
+        new Thread(consumer).start();
+
+        Thread.sleep(4000);
     }
 }
