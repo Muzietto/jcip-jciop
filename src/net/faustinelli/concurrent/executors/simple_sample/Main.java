76e3c0be599a25522b74a4fb246eca04c21f1012
Muzietto <marco_faustinelli@yahoo.com>
2016-03-29 09:04:06 +0200

    starting with threadpools

---
+++ src/net/faustinelli/concurrent/executors/simple_sample/Main.java
@@ -0,0 +1,32 @@
+package net.faustinelli.concurrent.executors.simple_sample;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by Marco Faustinelli (Muzietto) on 29/03/2016.
+ */
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService es1 = Executors.newFixedThreadPool(10);
+
+        es1.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Async task");
+            }
+        });
+
+        Future<String> fut = es1.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                String result = "Async string";
+                System.out.println(result);
+                return result;
+            }
+        });
+
+        System.out.println("gotten " + fut.get());
+
+        es1.shutdown();
+    }
+}
