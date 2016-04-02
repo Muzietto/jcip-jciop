9f1fa6ae3df06a35b8fed70af7094ee319926a08
Davide Bellettini <davide@bellettini.eu>
2016-04-02 17:04:05 +0200

    First feature printed

--- src/main/java/net/faustinelli/earthquakes/model/EarthquakeFeed.java
+++ src/main/java/net/faustinelli/earthquakes/model/EarthquakeFeed.java
@@ -4,6 +4,7 @@ import com.google.api.client.json.GenericJson;
 import com.google.api.client.util.Key;
 
 import java.util.List;
+import java.util.Properties;
 
 public class EarthquakeFeed {
     @Key
@@ -34,6 +35,30 @@ public class EarthquakeFeed {
 
         @Key
         private Integer count;
+
+        public Long getGenerated() {
+            return generated;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Integer getStatus() {
+            return status;
+        }
+
+        public String getApi() {
+            return api;
+        }
+
+        public Integer getCount() {
+            return count;
+        }
     }
 
     public static class Feature extends GenericJson {
@@ -41,7 +66,37 @@ public class EarthquakeFeed {
         @Key
         private String id;
 
+        @Key
+        private Properties properties;
 
+        public static class Properties {
+            @Key
+            private String url;
+
+            @Key
+            private String place;
+
+            public String getUrl() {
+                return url;
+            }
+
+            public String getPlace() {
+                return place;
+            }
+
+            @Override
+            public String toString() {
+                return getUrl() + ", " + getPlace();
+            }
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public Properties getProperties() {
+            return properties;
+        }
     }
 }
 
