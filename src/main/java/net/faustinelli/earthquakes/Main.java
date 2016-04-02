83b8b612c33fcf76d9601a6fb539a4b3cf31c958
Davide Bellettini <davide@bellettini.eu>
2016-04-02 18:56:08 +0200

    Implement DelayedFeature with delayed queue

--- src/main/java/net/faustinelli/earthquakes/Main.java
+++ src/main/java/net/faustinelli/earthquakes/Main.java
@@ -4,13 +4,20 @@ import com.google.api.client.http.*;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.gson.GsonFactory;
+import net.faustinelli.earthquakes.model.DelayedFeature;
 import net.faustinelli.earthquakes.model.EarthquakeFeed;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.Clock;
+import java.util.Date;
+import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
 
 public class Main {
-    public static void oldMain(String args[]) throws IOException {
+    public static void main1(String args[]) throws IOException {
 
         HttpTransport transport = new NetHttpTransport();
         GenericUrl url = new GenericUrl("http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-04-02&endtime=2016-04-03");
@@ -28,7 +35,7 @@ public class Main {
         }
     }
 
-    public static void main(String args[]) throws IOException {
+    public static void main2(String args[]) throws IOException {
         HttpTransport transport = new NetHttpTransport();
         GenericUrl url = new GenericUrl("http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-04-02&endtime=2016-04-03");
 
@@ -41,5 +48,46 @@ public class Main {
         EarthquakeFeed earthquakeFeed = response.parseAs(EarthquakeFeed.class);
 
         System.out.println(earthquakeFeed.getFeatures().get(0).toString());
+    }
+
+    public static void main(String[] args) throws IOException {
+        DelayQueue<DelayedFeature> queue = new DelayQueue<>();
+
+        HttpTransport transport = new NetHttpTransport();
+        GenericUrl url = new GenericUrl("http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-04-02&endtime=2016-04-03");
+
+        HttpRequestFactory factory = transport.createRequestFactory();
+        HttpRequest request = factory.buildGetRequest(url);
+        request.getHeaders().setAccept("application/json");
+        request.setParser(new JsonObjectParser(GsonFactory.getDefaultInstance()));
+
+        HttpResponse response = request.execute();
+        EarthquakeFeed earthquakeFeed = response.parseAs(EarthquakeFeed.class);
+
+        Clock clock = Clock.systemUTC();
+        // consumer
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        System.out.println(queue.take().unwrap());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        Random random = new Random();
+
+        earthquakeFeed.getFeatures().stream().forEach(x -> {
+            int size = queue.size();
+            int delaySeconds = size * 5 + random.nextInt(4);
+            long delay = delaySeconds * 1000L;
+
+            queue.put(new DelayedFeature(x, clock, new Date(clock.millis() + delay)));
+        });
     }
 }
---
+++ src/main/java/net/faustinelli/earthquakes/model/DelayedFeature.java
@@ -0,0 +1,32 @@
+package net.faustinelli.earthquakes.model;
+
+import java.time.Clock;
+import java.util.Date;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+public class DelayedFeature implements Delayed {
+    private final EarthquakeFeed.Feature feature;
+    private final Clock clock;
+    private final Date expiration;
+
+    public DelayedFeature(EarthquakeFeed.Feature feature, Clock clock, Date expiration) {
+        this.feature = feature;
+        this.clock = clock;
+        this.expiration = expiration;
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) {
+        return unit.convert(expiration.getTime() - clock.millis(), TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public int compareTo(Delayed o) {
+        return expiration.compareTo(((DelayedFeature)o).expiration);
+    }
+
+    public EarthquakeFeed.Feature unwrap() {
+        return feature;
+    }
+}
