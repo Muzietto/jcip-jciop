5f5a57e28a85972faca03c980a924bd02e26ca33
Muzietto <marco_faustinelli@yahoo.com>
2016-04-04 08:49:57 +0200

    licenses

--- src/main/java/net/faustinelli/earthquakes/model/EarthquakeFeed.java
+++ src/main/java/net/faustinelli/earthquakes/model/EarthquakeFeed.java
@@ -1,3 +1,11 @@
+/*
+ * Project: jcip-jciop
+ * Author: Marco Faustinelli - Muzietto (contacts@faustinelli.net), Davide Bellettini (http://about.bellettini.eu/)
+ * Web: http://faustinelli.wordpress.com/, http://www.github.com/muzietto, http://faustinelli.net/
+ * Version: 1.0
+ * The GPL 3.0 License - Copyright (c) 2015-2016 - The jcip-jciop Project
+ */
+
 package net.faustinelli.earthquakes.model;
 
 import com.google.api.client.json.GenericJson;
