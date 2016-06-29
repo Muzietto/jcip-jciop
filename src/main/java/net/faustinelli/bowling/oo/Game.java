d6059a110f1fbfb81a85e67a63bc8951668a5557
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-06-29 12:46:28 +0100

    Update Game.java

--- src/main/java/net/faustinelli/bowling/oo/Game.java
+++ src/main/java/net/faustinelli/bowling/oo/Game.java
@@ -3,38 +3,22 @@ package net.faustinelli.funkyJavaGym.net.faustinelli.bowling;
 public class Game {
 
     private Frame currentFrame;
-    private Frame previousFrame;
 
     public Game() {
         init();
     }
 
     public void init() {
-        previousFrame = currentFrame;
-        currentFrame = new Frame(previousFrame, this);
+        currentFrame = new Frame(currentFrame, this);
     }
 
     // how many pins did you bump?
     public void roll(int pins) {
-        if (gameIsOver()) {
+        if (currentFrame.gameIsOver()) {
             throw new RuntimeException("bowling game over!");
         }
 
         currentFrame.roll(pins);
-
-    }
-
-    private boolean gameIsOver() {
-        return numberOfFrames() > 10
-               && !(previousFrameWasSpare() || previousFrameWasStrike());
-    }
-
-    private boolean previousFrameWasStrike() {
-        return previousFrame != null && previousFrame.isStrike;
-    }
-
-    private boolean previousFrameWasSpare() {
-        return previousFrame != null && previousFrame.getScore() == 10 && !previousFrame.isStrike;
     }
 
     public int score() {
