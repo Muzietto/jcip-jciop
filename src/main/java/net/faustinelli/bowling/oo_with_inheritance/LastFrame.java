fb0f5875ae53d375c9e1e4780b6182a5f8b48770
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-06-29 17:27:09 +0100

    Update LastFrame.java

--- src/main/java/net/faustinelli/bowling/oo_with_inheritance/LastFrame.java
+++ src/main/java/net/faustinelli/bowling/oo_with_inheritance/LastFrame.java
@@ -2,7 +2,40 @@ package net.faustinelli.funkyJavaGym.net.faustinelli.bowling.oo_with_inheritance
 
 public class LastFrame extends Frame {
 
+    private boolean isThirdRoll = false;
+    private boolean gameOver = false;
+
     public LastFrame(Frame previousFrame, Game game) {
         super(previousFrame, game);
+    }
+
+    public void roll(int pins) {
+        updateScores(pins);
+        updateGameState(pins);
+    }
+
+    public boolean gameIsStillGoingOn() {
+        return !gameOver;
+    }
+
+    private void updateGameState(int pins) {
+        if (isFirstRoll) {
+            isFirstRoll = false;
+            isSecondRoll = true;
+            if (pins == 10) {
+                isStrike = true;
+            }
+        } else if (isSecondRoll) {
+            isSecondRoll = false;
+
+            if (!isStrike && score < 10) {
+                gameOver = true;
+            } else {
+                isThirdRoll = true;
+            }
+        } else if (isThirdRoll) {
+            isThirdRoll = false;
+            gameOver = true;
+        }
     }
 }
