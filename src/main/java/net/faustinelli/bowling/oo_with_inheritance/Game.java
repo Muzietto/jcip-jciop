97f40347f2f121b57e6ea87c2a36beaf94056b7d
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-06-29 12:52:18 +0100

    Create Game.java

---
+++ src/main/java/net/faustinelli/bowling/oo_with_inheritance/Game.java
@@ -0,0 +1,31 @@
+package net.faustinelli.funkyJavaGym.net.faustinelli.bowling;
+
+public class Game {
+
+    private Frame currentFrame;
+
+    public Game() {
+        init();
+    }
+
+    public void init() {
+        currentFrame = new Frame(currentFrame, this);
+    }
+
+    // how many pins did you bump?
+    public void roll(int pins) {
+        if (currentFrame.gameIsOver()) {
+            throw new RuntimeException("bowling game over!");
+        }
+
+        currentFrame.roll(pins);
+    }
+
+    public int score() {
+        return currentFrame.computeScore();
+    }
+
+    public int numberOfFrames() {
+        return currentFrame.numberOfFrames();
+    }
+}
