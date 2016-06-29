0a7e388e7c1c399113f373a5ca712612b41791fa
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-06-29 13:10:07 +0100

    Create LastFrame.java

---
+++ src/main/java/net/faustinelli/bowling/oo_with_inheritance/LastFrame.java
@@ -0,0 +1,8 @@
+package net.faustinelli.funkyJavaGym.net.faustinelli.bowling.oo_with_inheritance;
+
+public class LastFrame extends Frame {
+
+    public LastFrame(Frame previousFrame, Game game) {
+        super(previousFrame, game);
+    }
+}
