8b4c6a1b7474ff3941cd19b48221e107b54e59c3
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-09-28 08:49:46 +0100

    Update AsyncUnitTest.java

--- src/test/java/net/faustinelli/concurrent/asyncunit/AsyncUnitTest.java
+++ src/test/java/net/faustinelli/concurrent/asyncunit/AsyncUnitTest.java
@@ -2,53 +2,56 @@ package net.faustinelli.concurrent.asyncunit;
 
 import static org.junit.Assert.*;
 
-import net.faustinelli.lift.movement.Move;
-import net.faustinelli.lift.queue.Floor;
 import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 
 public class AsyncUnitTest {
 
-
-    private void waitOnCallback(MyCallback cb) {
-        synchronized (cb) {
-            try {
-                cb.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            assertTrue(false);
-        }
-    }
-
     @Test
-    public void testMoveTwoFloorsNoBlock() throws InterruptedException, ExecutionException {
+    public void testWaitNotifyAssert() throws InterruptedException {
 
-        Long now = System.currentTimeMillis();
-        System.out.println("Test thread says start TimeMillis=" + now);
+        Long start = System.currentTimeMillis();
+        System.out.println("Test thread says start TimeMillis=" + start);
 
-        Floor groundFloor = new Floor(0);
-        Floor secondFloor = new Floor(2);
+        MyCallback cb = new MyCallback();
+        assertFalse(cb.executed);
 
-        Move myMove = new Move(groundFloor);
-        MyCallback myCB = new MyCallback();
-
-        myMove.moveTo(secondFloor).thenRun(() -> {
-            myCB.timestamp();
-            myCB.onDone();
+        // clunky solution to move the work on another thread and introduce some delay
+        CompletableFuture cf = new CompletableFuture();
+        FutureTask ftask = new FutureTask(() -> {
+            System.out.println("Scheduled Task says end TimeMillis=" + System.currentTimeMillis());
+            // do any sort of useful job and modify cb's state accordingly
+            cb.execute();
+            cf.complete(null);
+            return "whatever";
         });
 
-        waitOnCallback(myCB);
+        Executors.newScheduledThreadPool(1).schedule(ftask, 500, TimeUnit.MILLISECONDS);
+        cf.thenRun(() -> {
+            cb.onDone();
+        });
 
-        System.out.println("Test thread says end TimeMillis=" + now);
+        synchronized (cb) {
+            cb.wait();
+            // put here assertions about cb state
+            assertTrue(cb.executed);
+        }
+        Long end = System.currentTimeMillis();
+        System.out.println("Test thread says end TimeMillis=" + end);
+        System.out.println("Elapsed time is millis=" + (end - start));
     }
 
     public class MyCallback {
 
-        public void timestamp() {
-            System.out.println("Callback says TimeMills=" + System.currentTimeMillis());
+        public Boolean executed = Boolean.FALSE;
+
+        public void execute() {
+            System.out.println("Callback says TimeMillis=" + System.currentTimeMillis());
+            executed = true;
         }
 
         public void onDone() {
