f84ae0c52792b54600c303ed2e917fbea6f06b1c
Marco Faustinelli <marco.faustinelli@edenred.com>
2025-04-09 11:46:17 +0200

    ready for action

--- src/main/java/net/faustinelli/checkout/product/Product.java
+++
@@ -1,8 +0,0 @@
-package main.java.net.faustinelli.checkout.product;
-
-import main.java.net.faustinelli.checkout.pricing.PricingRule;
-
-public interface Product {
-
-  public int unitaryPrice();
-}
